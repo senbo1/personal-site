c534750239c07c772f30fa4e7b0d1d427ab0b8e3
senbo1 <harshbhadu86@gmail.com>
2024-12-07 19:55:55 +0530

    add introduction

---
+++ src/components/Name.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { FC } from 'react';
+
+type NameProps = {};
+
+const Name: FC<NameProps> = () => {
+  return <h1 className="text-4xl font-bold mb-8 text-center">Harsh Bhadu</h1>;
+};
+
+export default Name;
