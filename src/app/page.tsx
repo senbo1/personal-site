0a382e72e98fb0ee93cdfb412928fc9c28f5eec6
senbo1 <harshbhadu86@gmail.com>
2024-12-10 21:07:49 +0530

    added BlurFade & ThemeToggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,42 @@
 import Contact from '@/components/Contact';
 import Name from '@/components/Name';
 import Projects from '@/components/Projects';
+import { ThemeToggle } from '@/components/Theme/ThemeToggle';
 import { Badge } from '@/components/ui/badge';
+import BlurFade from '@/components/ui/blur-fade';
+
+export const BLUR_FADE_DELAY = 0.04;
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-10 sm:p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
-        <Name />
-        <h2 className="text-2xl mb-4 text-center">Next.js Developer</h2>
+        <header>
+          <ThemeToggle />
+        </header>
+        <BlurFade delay={BLUR_FADE_DELAY}>
+          <Name />
+        </BlurFade>
+
+        <BlurFade delay={BLUR_FADE_DELAY * 2}>
+          <h2 className="text-2xl mb-4 text-center">Next.js Developer</h2>
+        </BlurFade>
 
         {/* Introduction */}
         <section className="mb-12">
-          <p className="text-xl text-center">
-            I love creating fast, responsive, and user-friendly web applications
-            using Next.js and React.
-          </p>
+          <BlurFade delay={BLUR_FADE_DELAY * 3}>
+            <p className="text-xl text-center">
+              I love creating fast, responsive, and user-friendly web
+              applications using Next.js and React.
+            </p>
+          </BlurFade>
         </section>
 
         {/* Skills */}
         <section className="mb-12">
-          <h3 className="text-2xl font-semibold mb-4">Skills</h3>
+          <BlurFade delay={BLUR_FADE_DELAY * 4}>
+            <h3 className="text-2xl font-semibold mb-4">Skills</h3>
+          </BlurFade>
           <div className="flex flex-wrap gap-2">
             {[
               'Next.js',
@@ -31,18 +47,20 @@ export default function Home() {
               'REST APIs',
               'PostgreSQL',
               'MongoDB',
-              'Git',
-            ].map((skill) => (
-              <Badge key={skill} variant="secondary">
-                {skill}
-              </Badge>
+              'Docker',
+            ].map((skill, id) => (
+              <BlurFade delay={BLUR_FADE_DELAY * 4 + id * 0.05} key={skill}>
+                <Badge variant="secondary">{skill}</Badge>
+              </BlurFade>
             ))}
           </div>
         </section>
 
         <Projects />
 
-        <Contact />
+        <BlurFade delay={BLUR_FADE_DELAY * 6}>
+          <Contact />
+        </BlurFade>
       </div>
     </main>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,6 +8,8 @@ import {
   CardHeader,
   CardTitle,
 } from './ui/card';
+import BlurFade from './ui/blur-fade';
+import { BLUR_FADE_DELAY } from '@/app/page';
 
 type Project = {
   title: string;
@@ -34,41 +36,45 @@ const projects = [
 const Projects = () => {
   return (
     <section className="mb-12">
-      <h3 className="text-2xl font-semibold mb-4">Projects</h3>
+      <BlurFade delay={BLUR_FADE_DELAY * 5}>
+        <h3 className="text-2xl font-semibold mb-4">Projects</h3>
+      </BlurFade>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {projects.map((project, index) => (
-          <Card key={index}>
-            <CardHeader>
-              <CardTitle>{project.title}</CardTitle>
-            </CardHeader>
-            <CardContent className="max-h-24 h-full">
-              <CardDescription>{project.description}</CardDescription>
-            </CardContent>
-            <CardFooter className="flex gap-4">
-              {project.demoUrl && (
+          <BlurFade delay={BLUR_FADE_DELAY * 5 + index * 0.05} key={index}>
+            <Card className="h-full">
+              <CardHeader>
+                <CardTitle>{project.title}</CardTitle>
+              </CardHeader>
+              <CardContent className="md:max-h-24 md:h-full">
+                <CardDescription>{project.description}</CardDescription>
+              </CardContent>
+              <CardFooter className="flex gap-4">
+                {project.demoUrl && (
+                  <Button variant="outline" size="sm" asChild>
+                    <a
+                      href={project.demoUrl}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <ExternalLink className="mr-2 h-4 w-4" />
+                      Demo
+                    </a>
+                  </Button>
+                )}
                 <Button variant="outline" size="sm" asChild>
                   <a
-                    href={project.demoUrl}
+                    href={project.codeUrl}
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <ExternalLink className="mr-2 h-4 w-4" />
-                    Demo
+                    <Github className="mr-2 h-4 w-4" />
+                    Code
                   </a>
                 </Button>
-              )}
-              <Button variant="outline" size="sm" asChild>
-                <a
-                  href={project.codeUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Github className="mr-2 h-4 w-4" />
-                  Code
-                </a>
-              </Button>
-            </CardFooter>
-          </Card>
+              </CardFooter>
+            </Card>
+          </BlurFade>
         ))}
       </div>
     </section>
