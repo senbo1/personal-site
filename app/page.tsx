804817c8c0958fe322bf3ae34dd4952cd7ffab3f
senbo1 <harshbhadu86@gmail.com>
2025-03-01 05:05:49 +0530

    add nav & introduction

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
-import Nav from '@/components/Nav';
+import { Nav } from '@/components/Nav';
+import { ThemeToggler } from '@/components/ui/ThemeToggler';
 
 export default function Home() {
   return (
     <>
       <Nav />
-      <main></main>
+      <main>
+        <section className="flex flex-col gap-2">
+          <h1 className="text-4xl font-bold">Hi, I am harsh</h1>
+          <h2 className="text-lg font-medium">
+            I love building <span className="italic text-red-500">fast</span>{' '}
+            and <span className="italic text-green-500">efficient</span> full
+            stack applications.
+          </h2>
+          <p>
+            Stack - TypeScript, Golang, Next.js, React, Node.js, PostgreSQL,
+            Prisma, Drizzle, Docker
+          </p>
+        </section>
+
+        <section className="pt-20">
+          <h2 className="text-xl text-center font-bold">Projects</h2>
+        </section>
+      </main>
     </>
   );
 }
